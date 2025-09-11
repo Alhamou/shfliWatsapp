4f5e5334757153a4e81317d2622f1e1d4e2ebc4f
alhamou <emadalhamo@gmail.com>
2025-09-11 13:11:55 +0200

    dev

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ const client = new Client({
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
     ]
   }
 });
